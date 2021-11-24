0ee6a1d1c9c16d66ca4ef06afd1888272b59d0f0
Joseph-Riva <josephariva@gmail.com>
2021-11-24 13:00:01 -0700

    restructure code to be in package heirarcht

--- hadoop_code/cs435/TP/TimeVariancePerWord.java
+++ hadoop_code/cs435/TP/TimeVariancePerWord.java
@@ -18,66 +18,31 @@ import java.util.List;
 import java.util.Arrays;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.lang.Math;
 
 public class TimeVariancePerWord {
 	public static class VarianceReducer extends Reducer<Text, FloatWritable, Text, Text>{
 
-		List<Word> sortedList;
 		@Override
-		protected void setup(Context context) {
-			sortedList = new ArrayList<Word>();
-		}
-
-		@Override
-		protected void reduce(Text key, Iterable<FloatWritable> values, Context context){
+		protected void reduce(Text key, Iterable<FloatWritable> values, Context context) throws IOException, InterruptedException {
+			//variance is calculated as s^2 = sum(math.pow(x-mean, 2))/n
 			int count = 0;
 			float totalTime = 0;
 			for(FloatWritable time : values){
 				count++;
 				totalTime += time.get();
 			}
-			//if(count > 99){
-			float avgTime = totalTime / count;
-			sortedList.add(new Word(key, avgTime, count));
-			//}
+
+			float mean = totalTime / count;
+			float var = 0;
+			for(FloatWritable time : values){
+				var += Math.pow(time.get()-mean, 2);
+			}
+			var = var / count;
+			context.write(key, new Text(var + ""));
 
 		}
 
-		@Override
-		protected void cleanup(Context context) throws IOException, InterruptedException {
-			Collections.sort(sortedList, new CustomComparator());
-			for(int i = 0; i < sortedList.size(); i++) {
-				context.write(new Text(sortedList.get(i).key), new Text(sortedList.get(i).avgTime + "\t" + sortedList.get(i).count));
-			}
-		}
-
-	}
-
-	public static class Word{
-		String key;
-		float avgTime;
-		int count;
-
-		Word(Text key, float time, int count){
-			this.key = key.toString();
-			this.avgTime = time;
-			this.count = count;
-		}
-	}
-
-	public static class CustomComparator implements Comparator<Word>{
-		@Override
-		public int compare(Word o1, Word o2){
-			if (o1.count > o2.count){
-				return -1;
-			}
-			else if(o1.count < o2.count){
-				return 1;
-			}
-			else{
-				return o1.key.compareTo(o2.key);
-			}
-		}
 	}
 
 

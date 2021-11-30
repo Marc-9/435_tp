ea03e34b5c4ede415f19c1b6cb38fdcd376ff0a4
Joseph Riva <josephariva@gmail.com>
2021-11-29 19:14:11 -0700

    update script

--- db/run_db.sh
+++ db/run_db.sh
@@ -4,12 +4,12 @@ IMAGE_NAME=speechtimerdb:latest
 docker build --rm -t $IMAGE_NAME .
 
 # kill old run 
-docker ps | grep 435_db
+docker ps -a | grep 435_db
 CONTAINER_RUNNING=$?
 
 if [ $CONTAINER_RUNNING -eq 0 ]; then
     docker stop 435_db
-    docker kill 435_db
+    docker container rm 435_db
     echo "killed running container"
 fi
 # run image

1b418a8aee0b055488a7c87c7973243777267d62
Joseph Riva <josephariva@gmail.com>
2021-11-29 17:03:07 -0700

    add db files

---
+++ db/run_db.sh
@@ -0,0 +1,18 @@
+IMAGE_NAME=speechtimerdb:latest
+
+# build image
+docker build --rm -t $IMAGE_NAME .
+
+# kill old run 
+docker ps | grep 435_db
+CONTAINER_RUNNING=$?
+
+if [ $CONTAINER_RUNNING -eq 0 ]; then
+    docker stop 435_db
+    docker kill 435_db
+    echo "killed running container"
+fi
+# run image
+docker run --name 435_db -d $IMAGE_NAME 
+
+docker ps

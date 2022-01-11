bf5f31902d995096005cc94ef26943b4cd771da7
JeanMarc <37602279+Marc-9@users.noreply.github.com>
2022-01-11 15:23:55 -0700

    annoying that I cant remove this, but its all local so...

--- api/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('DB_HOST')) define('DB_HOST', 'localhost');
-if (!defined('DB_USER')) define('DB_USER', 'root');
-if (!defined('DB_PASSWORD')) define('DB_PASSWORD', 'root');
-if (!defined('DB_DATABASE')) define('DB_DATABASE', 'speech_timer');
-?>
